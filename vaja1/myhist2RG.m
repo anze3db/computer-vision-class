2419d21cd1321be1c9cd562943c12038e160ebc6
Smotko <ensmotko@gmail.com>
2011-11-03 19:12:50 +0100

    Naloga 2

---
+++ vaja1/myhist2RG.m
@@ -0,0 +1,16 @@
+function h = myhist2RG(img1,bins)
+
+img1 = double(img1);
+img1=uint8(floor(img1*(bins)/255)+1);
+
+h=zeros(bins,bins);
+%execute the loop for each pixel in the image,
+
+for i = 1 : size(h,1)
+    for j = 1 : size(h,2)
+        h(i,j) = sum(sum(img1(:,:,1) == i & img1(:,:,2) == j));
+    end
+end
+
+h=h/sum(sum(sum(h)))
+
--- vaja1/vaja2.m
+++ vaja1/vaja2.m
@@ -1,22 +1,32 @@
-A = imread("data/graf.png");
-
-img = double(rgb2gray(A));
-clf; 
-%figure(1);
-%subplot(1,2,1); imshow(uint8(A));
-%subplot(1,2,2); imshow(uint8(img));
-figure(2);
+% a)
+img = rgb2gray(imread("data/graf.png"));
 
 [h,w] = size(img) ;
 img1D = reshape(img,w*h,1);
 
-bins = 10;
-h = hist(img1D,bins);
-subplot(1,3,1) ; bar(h) ;
-bins = 20;
-h = hist(img1D,bins);
-subplot(1,3,2) ; bar(h);
-bins = 40;
-h = hist(img1D,bins);
-subplot(1,3,3) ; plot(h);
+figure(1); clf;
+subplot(1,3,1); bar(hist(img1D,10));
+subplot(1,3,2); bar(hist(img1D,20));
+subplot(1,3,3); bar(hist(img1D,40));
+
+% b)
+figure(2); clf;
+% zakaj nista identična?
+subplot(1,3,1); bar(myhist(img1D,10));
+subplot(1,3,2); bar(myhist(img1D,20));
+subplot(1,3,3); bar(myhist(img1D,40));
+
+% c)
+img = double(imread("data/graf.png"));
+img(:,:,1) = (img(:,:,1)./(img(:,:,1).+img(:,:,2).+img(:,:,3))*255); % img_g
+img(:,:,2) = (img(:,:,2)./(img(:,:,1).+img(:,:,2).+img(:,:,3))*255); % img_r
+img(:,:,3) = zeros(size(img,1),size(img,2)); % img_b
+img= uint8(img);
+figure(3); clf; imshow(img);
+plot(myhist2RG(img,10))
+
+
+
+
+% figure(3); clf;
 
