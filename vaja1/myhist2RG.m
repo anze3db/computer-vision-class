a1cc26f94ffdd0ca8cff153084ef1b2b4817bbbd
Smotko <ensmotko@gmail.com>
2012-01-13 15:46:29 +0100

    AAA

--- vaja1/myhist2RG.m
+++ vaja1/myhist2RG.m
@@ -6,6 +6,10 @@ img1=uint8(floor(img1*(bins)/255)+1);
 h=zeros(bins,bins);
 %execute the loop for each pixel in the image,
 
+for i = 1 : bins
+    
+endfor
+
 for i = 1 : size(h,1)
     for j = 1 : size(h,2)
         h(i,j) = sum(sum(img1(:,:,1) == i & img1(:,:,2) == j));
